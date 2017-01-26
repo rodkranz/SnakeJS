96973487dd161d2a550931dec40191d1a338fa19
Rodrigo Lopes <dev.rodrigo.lopes@gmail.com>
2017-01-26 23:58:27 +0000

    add death for snake

--- assets/snake.js
+++ assets/snake.js
@@ -7,7 +7,7 @@ function Snake() {
     this.xSpeed = 1;
     this.ySpeed = 0;
     this.total  = 0;
-    this.tail = [];
+    this.tail   = [];
 
     this.update = function update() {
         if (this.total === this.tail.length) {
@@ -15,7 +15,7 @@ function Snake() {
                 this.tail[i] = this.tail[i + 1];
             }
         }
-        this.tail[this.total-1] = createVector(this.x, this.y);
+        this.tail[this.total - 1] = createVector(this.x, this.y);
 
         this.x = this.x + this.xSpeed * scl;
         this.y = this.y + this.ySpeed * scl;
@@ -45,5 +45,16 @@ function Snake() {
     this.dir = function dir(x, y) {
         this.xSpeed = x;
         this.ySpeed = y;
+    };
+
+    this.death = function death() {
+        for (var i = 0; i < this.tail.length; i++) {
+            var pos = this.tail[i];
+            var d   = dist(this.x, this.y, pos.x, pos.y);
+            if (d < 1) {
+                this.total = 0;
+                this.tail = [];
+            }
+        }
     }
 }
