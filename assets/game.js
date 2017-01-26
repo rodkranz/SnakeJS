4bea8c73905a5e1aad6596235b095c0de0799fc7
Rodrigo Lopes <dev.rodrigo.lopes@gmail.com>
2017-01-26 23:34:52 +0000

    add food and eat food

--- assets/game.js
+++ assets/game.js
@@ -2,32 +2,51 @@
  * Created by rlopes on 26/01/2017.
  */
 var snake;
+var scl = 20;
+var food;
 
 function setup() {
     createCanvas(600, 600);
-    s = new Snake();
+    snake = new Snake();
 
+    frameRate(10);
+    pickLocation();
+}
+
+function pickLocation() {
+    var cols = floor(width / scl);
+    var rows = floor(height / scl);
+
+    food = createVector(floor(random(cols)), floor(random(rows)));
+    food.mult(scl);
 }
 
 function draw() {
     background(51);
-    s.update();
-    s.show();
+    snake.update();
+    snake.show();
+
+    if (snake.eat(food)) {
+        pickLocation();
+    }
+
+    fill(255, 0, 100);
+    rect(food.x, food.y, scl, scl);
 }
 
 function keyPressed() {
     switch (keyCode) {
         case UP_ARROW:
-            s.dir(0, -1);
+            snake.dir(0, -1);
             break;
         case DOWN_ARROW:
-            s.dir(0, 1);
+            snake.dir(0, 1);
             break;
         case RIGHT_ARROW:
-            s.dir(1, 0);
+            snake.dir(1, 0);
             break;
         case LEFT_ARROW:
-            s.dir(-1, 0);
+            snake.dir(-1, 0);
             break
     }
 }
--- assets/snake.js
+++ assets/snake.js
@@ -2,19 +2,27 @@
  * Created by rlopes on 26/01/2017.
  */
 function Snake() {
-    this.x = 0;
-    this.y = 0;
+    this.x      = 0;
+    this.y      = 0;
     this.xSpeed = 1;
     this.ySpeed = 0;
 
     this.update = function update() {
-        this.x = this.x + this.xSpeed;
-        this.y = this.y + this.ySpeed;
+        this.x = this.x + this.xSpeed * scl;
+        this.y = this.y + this.ySpeed * scl;
+
+        this.x = constrain(this.x, 0, width - scl);
+        this.y = constrain(this.y, 0, height - scl);
+    };
+
+    this.eat = function eat(pos) {
+        var d = dist(this.x, this.y, pos.x, pos.y);
+        return (d < 1);
     };
 
     this.show = function show() {
         fill(255);
-        rect(this.x, this.y, 10, 10);
+        rect(this.x, this.y, scl, scl);
     };
 
     this.dir = function dir(x, y) {
